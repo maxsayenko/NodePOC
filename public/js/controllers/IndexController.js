44ce9ddc518c4d7de41625554d53479598dfde68
OpenShift guest <max.sayenko@gmail.com>
2013-12-26 18:40:16 -0500

    Adding some Angular, wiring views: Hogan+Angular

---
+++ public/js/controllers/IndexController.js
@@ -0,0 +1,9 @@
+function IndexController($scope)
+{
+    var model = {
+        name: "James",
+        lastName: "Bond"
+    };
+    
+    $scope.model=model;
+}
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,16 @@
 /*
  * GET home page.
  */
+ 
+ var app = require('express')();
+ 
+ var path = require('path');
+ var dirName = path.dirname(process.mainModule.filename);
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+    console.log(path.dirname(process.mainModule.filename));
+    console.log(app.locals);
+    console.log(path.join(app.locals.settings.views, 'views/index.hjs'));
+    res.render('index');
+    //res.sendfile(path.join(dirName, 'views/index.hjs'));
 };
